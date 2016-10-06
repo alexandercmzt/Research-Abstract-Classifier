5d3d082109d905f20bce43a8c1d08371f7c53f32
Alexander CM <alexandercmzt@gmail.com>
2016-10-06 07:54:22 -0400

    test script can now be used to instantly test 10+ classifiers' performance

--- test_run.py
+++ test_run.py
@@ -3,6 +3,12 @@ from sklearn import linear_model
 from sklearn.neural_network import MLPClassifier
 from sklearn.externals import joblib
 from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 import collections
 import numpy as np
 import csv
@@ -10,29 +16,6 @@ import csv
 X_train = joblib.load('saves/data/train_in.csv_feature_vectors.pkl')
 X_test = joblib.load('saves/data/test_in.csv_feature_vectors.pkl')
 y_train = joblib.load('saves/data/train_out.csv_y_vector.pkl')
-
-
-def accuracy(gold, predict):
-    assert len(gold) == len(predict)
-    corr = 0
-    for i in xrange(len(gold)):
-        if gold[i] == predict[i]:
-            corr += 1
-    acc = float(corr) / len(gold)
-    print 'Accuracy %d / %d = %.4f' % (corr, len(gold), acc)
-
-print collections.Counter(y_train)
-# clf = linear_model.LogisticRegression()
-# scores = cross_val_score(clf, X_train, y_train, cv=3)
-# print("LOGREG Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-# clf = svm.SVC()
-# scores = cross_val_score(clf, X_train, y_train, cv=3)
-# print("SVM Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-# clf = MLPClassifier(solver='lbgfs', alpha=1e-5,hidden_layer_sizes=(50,20,5), random_state=1)
-# scores = cross_val_score(clf, X_train, y_train, cv=3)
-# print("MLP Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-
 X_train = X_train.tolist()
 y_train = y_train.tolist()
 X = []
@@ -44,6 +27,46 @@ for i in xrange(len(y_train)):
 X_train = np.array(X)
 y_train = np.array(y)
 
+def accuracy(gold, predict):
+    assert len(gold) == len(predict)
+    corr = 0
+    for i in xrange(len(gold)):
+        if gold[i] == predict[i]:
+            corr += 1
+    acc = float(corr) / len(gold)
+    print 'Accuracy %d / %d = %.4f' % (corr, len(gold), acc)
+
+clf = linear_model.LogisticRegression()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("LOGREG Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = svm.SVC(kernel="linear")
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("SVM Accuracy (linear kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = svm.SVC(kernel="sigmoid")
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("SVM Accuracy (sigmoid kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = svm.SVC()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("SVM Accuracy (rbf kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = KNeighborsClassifier()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = BaggingClassifier(KNeighborsClassifier(),max_samples=0.5, max_features=0.5)
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("Bagging kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = RandomForestClassifier()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = ExtraTreesClassifier()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("Extremely Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+clf = AdaBoostClassifier()
+scores = cross_val_score(clf, X_train, y_train, cv=3)
+print("AdaBoost Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+
+
+
+
 # partition = -1*int(len(y_train)*(0.995))
 # clf = linear_model.LogisticRegression()
 # clf.fit(X_train[:partition], y_train[:partition])
@@ -52,15 +75,15 @@ y_train = np.array(y)
 # accuracy(gold,predictions)
 
 
-clf = linear_model.LogisticRegression()
-clf.fit(X_train,y_train)
-predictions = clf.predict(X_test)
-output = [['id', 'category']]
-for i in xrange(len(predictions)):
-	output.append([i, predictions[i]])
+# clf = linear_model.LogisticRegression()
+# clf.fit(X_train,y_train)
+# predictions = clf.predict(X_test)
+# output = [['id', 'category']]
+# for i in xrange(len(predictions)):
+# 	output.append([i, predictions[i]])
 
-with open("output1.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(output)
+# with open("output1.csv", "wb") as f:
+#     writer = csv.writer(f)
+#     writer.writerows(output)
 
 
