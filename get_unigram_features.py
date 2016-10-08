2dd8ddcadbb7e75a4594e8bfd11c763de0bceefe
Alexander CM <alexandercmzt@gmail.com>
2016-10-08 06:12:39 -0400

    Unigrams now stable

--- get_unigram_features.py
+++ get_unigram_features.py
@@ -6,72 +6,85 @@ import numpy as np
 import sklearn
 import string
 from sklearn.feature_extraction import DictVectorizer, FeatureHasher
+from sklearn.feature_extraction.text import TfidfTransformer
 from collections import Counter
 from sklearn.externals import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
 
-if len(sys.argv) != 3:
-	print "Please use the path to the train.csv file as the first command line argument and the path to the test.csv file as the second command line argument"
-	exit()
-filename = sys.argv[1]
-test_filename = sys.argv[2]
 
-if os.path.isfile('saves/' + filename +'_lemmatized_features.pkl') and os.path.isfile('saves/' + test_filename +'_lemmatized_features.pkl'):
-	print "Lemmatized words found, loading..."
-	data = joblib.load('saves/' + filename +'_lemmatized_features.pkl')
-	test_data = joblib.load('saves/' + test_filename +'_lemmatized_features.pkl')
-	print "Done loading."
-else:
-	print "Lemmatized words not found, creating and saving..."
-	with open(filename, 'rU') as f:
-		reader = csv.reader(f)
-		data = list(list(elem) for elem in csv.reader(f, delimiter=','))
-	f.close()
-	#remove headers from table
-	data = data[1:] 
+def get_unigrams(filename, test_filename, n=10000):
+	if os.path.isfile('saves/' + filename +'_lemmatized_features.pkl') and os.path.isfile('saves/' + test_filename +'_lemmatized_features.pkl'):
+		print "Lemmatized words found, loading..."
+		data = joblib.load('saves/' + filename +'_lemmatized_features.pkl')
+		test_data = joblib.load('saves/' + test_filename +'_lemmatized_features.pkl')
+		print "Done loading."
+	else:
+		print "Lemmatized words not found, creating and saving..."
+		with open(filename, 'rU') as f:
+			reader = csv.reader(f)
+			data = list(list(elem) for elem in csv.reader(f, delimiter=','))
+		f.close()
+		#remove headers from table
+		data = data[1:] 
 
-	with open(test_filename, 'rU') as f:
-		reader = csv.reader(f)
-		test_data = list(list(elem) for elem in csv.reader(f, delimiter=','))
-	f.close()
-	#remove headers from table
-	test_data = test_data[1:] 
+		with open(test_filename, 'rU') as f:
+			reader = csv.reader(f)
+			test_data = list(list(elem) for elem in csv.reader(f, delimiter=','))
+		f.close()
+		#remove headers from table
+		test_data = test_data[1:] 
 
-	def get_tokens(s):
-	    retval = []
-	    sents = sent_tokenize(s)
-	    for sent in sents:
-	        tokens = word_tokenize(sent)
-	        retval.extend(tokens)
-	    return retval
+		def get_tokens(s):
+		    retval = []
+		    sents = sent_tokenize(s)
+		    for sent in sents:
+		        tokens = word_tokenize(sent)
+		        retval.extend(tokens)
+		    return retval
 
-	#make all text lowercase and tokenize strings, then lemmatize words
-	wnl = WordNetLemmatizer()
+		#make all text lowercase and tokenize strings, then lemmatize words
+		wnl = WordNetLemmatizer()
+		for i in xrange(len(data)):
+			data[i] = map(str,map(wnl.lemmatize, (word_tokenize(data[i][1].lower().translate(None, string.punctuation)))))
+		for i in xrange(len(test_data)):
+			test_data[i] = map(str,map(wnl.lemmatize, (word_tokenize(test_data[i][1].lower().translate(None, string.punctuation)))))
+
+		joblib.dump(data, 'saves/' + filename +'_lemmatized_features.pkl')
+		joblib.dump(test_data, 'saves/' + test_filename +'_lemmatized_features.pkl')
+		print "Lemmatized words saved."
+
+	print "Counting tokens"
 	for i in xrange(len(data)):
-		data[i] = map(str,map(wnl.lemmatize, (word_tokenize(data[i][1].lower().translate(None, string.punctuation)))))
+		data[i] = Counter(data[i])
 	for i in xrange(len(test_data)):
-		test_data[i] = map(str,map(wnl.lemmatize, (word_tokenize(test_data[i][1].lower().translate(None, string.punctuation)))))
+		test_data[i] = Counter(test_data[i])
 
-	joblib.dump(data, 'saves/' + filename +'_lemmatized_features.pkl')
-	joblib.dump(test_data, 'saves/' + test_filename +'_lemmatized_features.pkl')
-	print "Lemmatized words saved."
-
-for i in xrange(len(data)):
-	data[i] = Counter(data[i])
-for i in xrange(len(test_data)):
-	test_data[i] = Counter(test_data[i])
-
-vec = FeatureHasher(n_features = 1000)
-unigram = vec.fit_transform(data).toarray()
-unigram_test = vec.transform(test_data).toarray()
-
-print unigram.shape
-print unigram_test.shape
-joblib.dump(unigram, 'saves/' + filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl')
-joblib.dump(unigram_test, 'saves/' + test_filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl')
-print "Saved feature matrix to " + 'saves/' + filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl'
-print "Saved test feature matrix to " + 'saves/' + test_filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl'
+	print "Hashing"
+	vec = FeatureHasher(n_features = n)
+	#vec = DictVectorizer()
+	tfidf = TfidfTransformer()
+	unigram = tfidf.fit_transform(vec.fit_transform(data).toarray()).toarray()
+	unigram_test = tfidf.transform(vec.transform(test_data).toarray()).toarray()
+	print unigram.shape
+	print unigram_test.shape
+	return unigram, unigram_test
 
 
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "Please use the path to the train.csv file as the first command line argument and the path to the test.csv file as the second command line argument"
+		exit()
+	filename = sys.argv[1]
+	test_filename = sys.argv[2]
+	unigram, unigram_test = get_unigrams(filename, test_filename)
+	print unigram.shape
+	print unigram_test.shape
+	joblib.dump(unigram, 'saves/' + filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl')
+	joblib.dump(unigram_test, 'saves/' + test_filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl')
+	print "Saved feature matrix to " + 'saves/' + filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl'
+	print "Saved test feature matrix to " + 'saves/' + test_filename +'_feature_vectors_' + 'UNIGRAMS' + '.pkl'
 
 
 
--- test_run.py
+++ test_run.py
@@ -9,35 +9,43 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from get_unigram_features import get_unigrams
 import collections
 import numpy as np
 import csv
 
-X_train = joblib.load('saves/data/train_in.csv_feature_vectors_800.pkl')
-X_test = joblib.load('saves/data/test_in.csv_feature_vectors_800.pkl')
-UNIGRAM_train = joblib.load('saves/data/train_in.csv_feature_vectors_UNIGRAMS.pkl')
-UNIGRAM_test = joblib.load('saves/data/test_in.csv_feature_vectors_UNIGRAMS.pkl')
-X_train_new = []
-X_test_new = []
-for i in xrange(len(X_train)):
-	X_train_new.append( np.concatenate([X_train[i], UNIGRAM_train[i]]).tolist() )
-for i in xrange(len(X_test)):
-	X_test_new.append( np.concatenate([X_test[i], UNIGRAM_test[i]]).tolist() )
-X_train = X_train_new
-X_test = X_test_new
-y_train = joblib.load('saves/data/train_out.csv_y_vector.pkl')
-y_train = y_train.tolist()
-X = []
-y = []
-for i in xrange(len(y_train)):
-	if y_train[i] != 'category':
-		X.append(X_train[i])
-		y.append(y_train[i])
-X_train = np.array(X)
-y_train = np.array(y)
-X_test = np.array(X_test)
-print "Feature fixes done "
-print X_train.shape, X_test.shape, y_train.shape
+
+def prepare_features(d2v_num, unigram_num):
+	y_train = joblib.load('saves/data/train_out.csv_y_vector.pkl')
+	if d2v_num != '0':
+		X_train = joblib.load('saves/data/train_in.csv_feature_vectors_' + d2v_num + '.pkl')
+		X_test = joblib.load('saves/data/test_in.csv_feature_vectors_' + d2v_num + '.pkl')
+		if unigram_num != 0:
+			UNIGRAM_train, UNIGRAM_test = get_unigrams('data/train_in.csv', 'data/test_in.csv', n=unigram_num)
+			X_train = np.concatenate((X_train, UNIGRAM_train), axis=1)
+			X_test = np.concatenate((X_test, UNIGRAM_test), axis=1)
+			print "Full features prepared"
+		else:
+			X_train = X_train.tolist()
+			X_test = X_test.tolist()
+	else: 
+		X_train, X_test = get_unigrams('data/train_in.csv', 'data/test_in.csv', n=unigram_num)
+		print "Back in test run"
+		return X_train, y_train, X_test
+	# y_train = y_train.tolist()
+	# i=0
+	# while i < len(y_train):
+	# 	if y_train[i] == 'category':
+	# 		del X_train[i]
+	# 		del y_train[i]
+	# 	else:
+	# 		i += 1
+	# print "Bugged features removed"
+	# X_train = np.array(X_train)
+	# y_train = np.array(y_train)
+	# X_test = np.array(X_test)
+	print X_train.shape, X_test.shape, y_train.shape
+	return X_train, y_train, X_test
 
 def accuracy(gold, predict):
     assert len(gold) == len(predict)
@@ -48,69 +56,88 @@ def accuracy(gold, predict):
     acc = float(corr) / len(gold)
     print 'Accuracy %d / %d = %.4f' % (corr, len(gold), acc)
 
-clf = LogisticRegression()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("LOGREG Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = svm.LinearSVC()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("SVM Accuracy (linear kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = AdaBoostClassifier(base_estimator=LogisticRegression())
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("AdaBoost (Log Reg) Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = AdaBoostClassifier()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("AdaBoost (Decision Trees) Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = svm.SVC(kernel="sigmoid")
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("SVM Accuracy (sigmoid kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = svm.SVC()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("SVM Accuracy (rbf kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = RandomForestClassifier()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = ExtraTreesClassifier()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("Extremely Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = KNeighborsClassifier()
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
-clf = BaggingClassifier(KNeighborsClassifier(),max_samples=0.5, max_features=0.5)
-scores = cross_val_score(clf, X_train, y_train, cv=3)
-print("Bagging kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+
+def cross_validate(X_train, y_train, X_test):
+
+	clf = LogisticRegression()
+	scores = cross_val_score(clf, X_train, y_train, cv=3)
+	print("LOGREG Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+
+	clf = svm.LinearSVC()
+	scores = cross_val_score(clf, X_train, y_train, cv=3)
+	print("SVM Accuracy (linear kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+
+	# clf = AdaBoostClassifier(base_estimator=LogisticRegression())
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("AdaBoost (Log Reg) Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = AdaBoostClassifier()
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("AdaBoost (Decision Trees) Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = svm.SVC(kernel="sigmoid")
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("SVM Accuracy (sigmoid kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = svm.SVC()
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("SVM Accuracy (rbf kernel): %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = RandomForestClassifier()
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = ExtraTreesClassifier()
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("Extremely Random Forests Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = KNeighborsClassifier()
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
+	
+	# clf = BaggingClassifier(KNeighborsClassifier(),max_samples=0.5, max_features=0.5)
+	# scores = cross_val_score(clf, X_train, y_train, cv=3)
+	# print("Bagging kNN Accuracy: %0.8f (+/- %0.8f)" % (scores.mean(), scores.std() * 2))
 
 
-# partition = -1*int(len(y_train)*(0.995))
-# clf = LogisticRegression()
-# clf.fit(X_train[:partition], y_train[:partition])
-# predictions = clf.predict(X_train[partition:])
-# gold = y_train[partition:]
-# accuracy(gold,predictions)
+def make_output(X_train, y_train, X_test):
+	clf = svm.LinearSVC()
+	clf.fit(X_train,y_train)
+	predictions = clf.predict(X_test)
+	output = [['id', 'category']]
+	for i in xrange(len(predictions)):
+		output.append([i, predictions[i]])
+	with open("output_40000.csv", "wb") as f:
+	    writer = csv.writer(f)
+	    writer.writerows(output)
 
-# clf = svm.LinearSVC()
-# clf.fit(X_train,y_train)
-# predictions = clf.predict(X_test)
-# output = [['id', 'category']]
-# for i in xrange(len(predictions)):
-# 	output.append([i, predictions[i]])
-# with open("output_1600_SVM.csv", "wb") as f:
-#     writer = csv.writer(f)
-#     writer.writerows(output)
+def compare(a,b):
+	with open(a, 'rU') as f:
+		reader = csv.reader(f)
+		data1 = list(list(elem) for elem in csv.reader(f, delimiter=','))
+		f.close()
+	with open(b, 'rU') as f:
+		reader = csv.reader(f)
+		data2 = list(list(elem) for elem in csv.reader(f, delimiter=','))
+		f.close()
+	lol = len(data1)
+	correct = 0
+	for i in xrange(lol):
+		if data1[i] == data2[i]:
+			correct+=1
+	print correct
+	print lol
 
+if __name__ == "__main__":
+	#compare('output_800_SVM.csv', 'output_400_SVM.csv')
 
-# with open('output_800_SVM.csv', 'rU') as f:
-# 	reader = csv.reader(f)
-# 	data1 = list(list(elem) for elem in csv.reader(f, delimiter=','))
-# 	f.close()
-# with open('output_400_SVM.csv', 'rU') as f:
-# 	reader = csv.reader(f)
-# 	data2 = list(list(elem) for elem in csv.reader(f, delimiter=','))
-# 	f.close()
-# lol = len(data1)
-# correct = 0
-# for i in xrange(lol):
-# 	if data1[i] == data2[i]:
-# 		correct+=1
+	# for d2v_num in ['0']:#,'200','400', '800']:
+	# 	for unigram_num in [50000, 75000, 100000, 150000]:
+	# 		print "------RUN FOR D2V[" + d2v_num + "] UNIGRAM[" + str(unigram_num) + "]------"
+	# 		X_train, y_train, X_test = prepare_features(d2v_num, unigram_num)
+	# 		cross_validate(X_train, y_train, X_test)
 
-# print correct
-# print lol
+	x,y,z = prepare_features('0', 40000)
+	# cross_validate(x,y,z)
+	make_output(x,y,z)
+
