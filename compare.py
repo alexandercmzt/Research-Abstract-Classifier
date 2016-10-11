a56bb845bd18cc42db52f8b5abd18739eda6ebb7
Shao Kun Deng <deng_shaokun@hotmail.com>
2016-10-11 10:38:17 -0400

    Add argument parser in lstm modules to facilitate usage

--- compare.py
+++ compare.py
@@ -1,11 +1,17 @@
 from __future__ import division
 
-file1 = open('testLSTM800.csv')
-file2 = open('testLSTM400.csv')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('files', nargs=2)
+args = parser.parse_args()
+
+file1 = open(args.files[0])
+file2 = open(args.files[1])
 
 l1 = file1.readlines()
 l2 = file2.readlines()
-# print l1[:10]
+
 count = 0
 for i in xrange(len(l1)):
 	if l1[i] != l2[i]:
