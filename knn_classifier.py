6701b69f471551e0c0a115736a50945515edf3d6
Tristan Struthers <Tristan@wpa167015.wireless.mcgill.ca>
2016-10-10 23:57:33 -0400

    Implemented matrix operations to speed up prediction time

--- knn_classifier.py
+++ knn_classifier.py
@@ -7,10 +7,11 @@ import collections
 import numpy as np
 import csv
 import math
+import time
 
-X_train = joblib.load('saves/data/train_in.csv_feature_vectors_800.pkl').tolist()
-X_test = joblib.load('saves/data/test_in.csv_feature_vectors_800.pkl').tolist()
-y_train = joblib.load('saves/data/train_out.csv_y_vector.pkl').tolist()
+X_train = joblib.load('saves/data/train_in.csv_feature_vectors_800.pkl')#.tolist()
+X_test = joblib.load('saves/data/test_in.csv_feature_vectors_800.pkl')#.tolist()
+y_train = joblib.load('saves/data/train_out.csv_y_vector.pkl')#.tolist()
 
 def accuracy(gold, predict):
     assert len(gold) == len(predict)
@@ -30,24 +31,40 @@ class knn_classifier(object):
 
 	def predict(self, inputVector):
 		""" Finds self.numNeighbors nearest neighbors to the inputVector and returns predicted class """
-		inputVector = np.asarray(inputVector)
+		# inputVector = np.asarray(inputVector)
+		start = time.time()
+		times = []
 		distances = []
 		idx = 0
+
+		# slow code, due to using python iterator
+		"""
 		for vector in X_train: # calculate distance between inputVector and every vector in the dataset
 			dist = np.linalg.norm(np.asarray(vector) - inputVector) # euclidian distance in np
-			
 			distances.append((dist, idx))
+			idx += 1
+		"""
+
+		# matrix subtraction that calculates all distances in a single line using numPy operations
+		distVect = np.linalg.norm(X_train - inputVector, axis=1)
+
+		for distValue in distVect:
+			distances.append((distValue, idx))
 			idx += 1
 		
 		distances = sorted(distances, key = lambda distance: distance[0]) # sort vectors in dataset by distance
+
 		res = distances[:self.numNeighbors]
 		neighbs = []
 		for aResult in res[1:]:
 			neighbs.append(self.class_to_label[y_train[aResult[1]]])
+			times.append(time.time() - start)
 
+		avgTime = sum(times)/len(times)
+		print 'Mean prediction time is %f seconds' % avgTime
 		return self.label_to_class[mode(np.asarray(neighbs))[0][0]]
 
-numExamples = 300
+numExamples = 1000
 
 # our classifier
 tristan_knn = knn_classifier(3)
@@ -58,7 +75,8 @@ for trainingExample in X_train[:numExamples]:
 	predictions.append(tristan_knn.predict(trainingExample))
 	counter += 1
 
-print 'accuracy of our classifier is %d.' % accuracy(y_train[:numExamples], predictions)
+print 'our knn model:'
+accuracy(y_train[:numExamples], predictions)
 
 
 # sklearn comparison
@@ -72,9 +90,5 @@ for trainingExample in X_train[:numExamples]:
 	predictions.append(neigh.predict([X_train[1]])[0])
 	counter += 1
 
-print 'accuracy of the sklearn classifier is %d.' % accuracy(y_train[:numExamples], predictions)
-
-
-
-
-
+print 'sklearn model:'
+accuracy(y_train[:numExamples], predictions)
